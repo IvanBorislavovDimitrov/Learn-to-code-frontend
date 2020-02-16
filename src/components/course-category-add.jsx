754c2d9b5779341b5c103f2c8ee3f87839f2e753
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2020-02-16 14:15:46 +0200

    Add logic for course-category adding

--- src/components/course-category-add.jsx
+++ src/components/course-category-add.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Component } from 'react';
+import { Redirect } from "react-router-dom";
+import qs from 'qs'
 
 class AddCourseCategory extends Component {
     state = {
@@ -9,15 +11,16 @@ class AddCourseCategory extends Component {
         return (
             <React.Fragment>
                 <div className="col-xl-3 col-lg-6 col-md-8 col-sm-10 mx-auto text-center form p-4">
-                    <form onSubmit={this.loginUser}>
+                    <form onSubmit={this.addCourseCategory}>
                         <div className="form-group">
                             <h3 htmlFor="exampleInputEmail1">Course Category Name</h3>
                             <input
-                                name="username"
+                                name="courseCategoryName"
                                 type="text"
                                 className="form-control"
                                 id="usernameInputField"
                                 placeholder="Course Category Name"
+                                onChange={this.changeInputField}
                             />
                         </div>
                         <button type="submit" className="btn btn-dark">
@@ -28,6 +31,45 @@ class AddCourseCategory extends Component {
             </React.Fragment>
         )
     }
+
+    addCourseCategory = event => {
+        event.preventDefault();
+        const currentThis = this;
+
+        async function add() {
+            const addCourseCategoryResponse = await fetch('http://localhost:8080/course-categories/add', {
+                method: 'POST',
+                mode: 'cors',
+                cache: 'no-cache',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: currentThis.state.courseCategoryName,
+                })
+            });
+            return addCourseCategoryResponse;
+        }
+
+        add().then(async respone => {
+            if (respone.status === 200) {
+                const responseJson = await respone.json();
+                this.props.history.push('/');
+                window.location.reload();
+            } else {
+                alert("Something went wrond. Check the console for error.");
+            }
+        });
+    };
+
+    changeInputField = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
 }
+
+
 
 export default AddCourseCategory;
